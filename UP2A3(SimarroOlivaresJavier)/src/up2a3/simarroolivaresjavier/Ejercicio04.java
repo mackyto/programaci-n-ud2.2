4a858892604440e3647b1acce90e24272090350e
Javier Simarro Olivares <javsimoli@alu.edu.gva.es>
2025-10-08 19:28:18 +0200

    prue

--- UP2A3(SimarroOlivaresJavier)/src/up2a3/simarroolivaresjavier/Ejercicio04.java
+++ UP2A3(SimarroOlivaresJavier)/src/up2a3/simarroolivaresjavier/Ejercicio04.java
@@ -16,13 +16,26 @@ import java.util.Scanner;
  */
 public class Ejercicio04 {
     
-    Scanner keyld =new Scanner(System.in);
+    public static void main(String [] args){
     
-    System.err.println("Por favor introduce solo números con comas para decimales o dará error");
-    System.out.println("Se va a proceder a comparar dos precios con hasta dos decimales he indicar si son iguales");
-    System.out.printf("\n\n");
+        Scanner keyld =new Scanner(System.in);
     
-    System.out.print("Introduzca el primer precio ")
-    double precio1=keyld.ne
+        System.err.println("Por favor introduce solo números con comas para decimales o dará error");
+        System.out.println("Se va a proceder a comparar dos precios con hasta dos decimales he indicar si son iguales");
     
+        System.out.printf("\n\n");    
+        System.out.print("Introduzca el primer precio ");
+        double precio1 = keyld.nextDouble();
+    
+        System.out.printf("\n");
+        System.out.print("Introduzca el segundo precio ");
+        double precio2 = keyld.nextDouble();
+    
+        System.out.printf("\n\nLos precios de %.2f", precio1); 
+        System.out.printf(" y %.2f", precio2);
+        System.out.println(" introducidos, son " + (precio1==precio2 ? "iguales":"diferentes")+"."); 
+        System.out.printf("\n\n");
+                
+    }
+            
 }
